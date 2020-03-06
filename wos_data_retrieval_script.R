d68bebc15bf0ab627056946e17f29aa5bd0b4e6d
sheila <sms493@cornell.edu>
2020-03-06 16:54:23 -0500

    added to do list

--- wos_data_retrieval_script.R
+++ wos_data_retrieval_script.R
@@ -275,3 +275,8 @@ pao_all_searches_pubs_raw <- bind_rows(pao_pubs_raw, pao_wwtp_pubs_raw,
 # export full dataset
 write_csv(x = pao_all_searches_pubs_raw, path = paste0(tabular_raw_data_path, "pao_all_searches_pubs_raw.csv"))
 
+
+# ---- TO DO LIST ----
+
+# TODO redo wwtp search for "enhanced biological phosphorus removal" and "batch reactor"
+
